6ab1dc27714a86e3623265a1c0a8efc5a75f886a
laurenhatchett <laurenhatchett@gmail.com>
2023-01-27 17:00:28 -0500

    set up components

---
+++ src/components/Game.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Game(){
+  <div>
+    Game
+  </div>
+}
+
+export default Game
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Square(){
+  <div>
+    Square
+  </div>
+}
+
+export default Square
