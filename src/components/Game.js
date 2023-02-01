136190347ae3843e98ed17a992ebf3e02aea41cf
laurenhatchett <laurenhatchett@gmail.com>
2023-01-31 23:59:37 -0500

    create state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
+import { calculateWinner } from "../helpers"
 import Board from "./Board"
 
 
 
 const Game = () => {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xIsNext, setXisNext] = useState(true)
+  const winner = calculateWinner(board)
 
   const handleClick = () => {
 
